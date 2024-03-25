444fa546d67204909bc5a62f4572f581134aaaa2
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-03-26 02:12:44 +0600

    Add model `User` which extends `AbstractUser`
    * Add migration for model `User`
    * Add `User` to admin
    * Set `AUTH_USER_MODEL` to `store.User` in project settings

--- store/models.py
+++ store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    class UserType(models.TextChoices):
+        BUYER = "BUYER", _("Buyer")
+        SELLER = "SELLER", _("Seller")
+
+    role = models.CharField(
+        max_length=8,
+        choices=UserType,
+        default=UserType.SELLER
+    )
