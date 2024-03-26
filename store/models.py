76b36d8c8bbe7a67a551908821312be7d067619b
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-03-26 17:01:55 +0600

    Remove method `User.save()`
    Add remarks about `upload_to`

--- store/models.py
+++ store/models.py
@@ -14,14 +14,13 @@ class User(AbstractUser):
         default=UserType.SELLER
     )
 
-    def save(self, *args, **kwargs):
-        self.set_password(self.password)
-        return super().save(*args, **kwargs)
-
 
 class Product(models.Model):
     name = models.CharField(max_length=64, null=False, blank=False)
     price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    # `upload_to` here refers to a firectory inside the project `ecommerce`
+    # but outside of app `store`
     image = models.ImageField(upload_to="files/images")
 
     def __str__(self) -> str:
