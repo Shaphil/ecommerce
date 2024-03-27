fe108b43cae3902373a9f89869edcfdb824ad37f
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2024-03-27 14:42:32 +0600

    Add model `Cart`, Update model `CartItem`
    * Add model `Cart` to admin
    * Add migrations for `Cart`

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from store.models import User, Product, CartItem
+from store.models import User, Product, CartItem, Cart
 
 admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(CartItem)
+admin.site.register(Cart)
--- store/models.py
+++ store/models.py
@@ -24,22 +24,29 @@ class Product(models.Model):
     image = models.ImageField(upload_to="files/images")
 
     def __str__(self) -> str:
-        return f"""Name: {self.name}
-        Price: {self.price}
-        Image: {self.image}
-        """
+        return f"Product: {self.name} - Price: {self.price} - Image: {self.image}"
 
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="products")
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=9, decimal_places=2, blank=True)
+    cart = models.ForeignKey("Cart", on_delete=models.CASCADE, related_name="cart", null=True, blank=True)
 
     def save(self, *args, **kwargs):
         self.price = self.quantity * self.product.price
         super().save()
 
+    def __str__(self):
+        return f"Item: {self.product.name} - Quantity: {self.quantity} - Price: {self.price}"
 
-# class Order(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
-#     items = models.ManyToManyField(CartItem, related_name="items", blank=True)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    items = models.PositiveIntegerField(default=0)
+    total_price = models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"User: {self.user.username} - Total: {self.total_price}"
