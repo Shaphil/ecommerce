aa0fe84de9290712dfb9012d1f1811dd6db2ed26
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2024-03-27 16:14:24 +0600

    Update `MEDIA_ROOT`
    Add TODO

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-_(7oq$i=w!&94xmyo$@dl2uu274&=8t!k%uh#irflg&a2bssrl
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 AUTH_USER_MODEL = 'store.User'
 
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -114,13 +109,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
 # ? `MEDIA_ROOT` is the absolute path to the directory containing media
+# TODO: Add dotenv to store these settings
 # MEDIA_ROOT = 'C:\\MEGA\\Playground\\InterviewProjects\\PotentialAI\\ecommerce\\'
 MEDIA_ROOT = '/home/mahmud/GitHub/shaphil/repos/ecommerce'
 MEDIA_URL = ''
