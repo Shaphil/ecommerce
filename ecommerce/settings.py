03afd75c9541aa61568155f419697844b8167e59
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2024-03-28 02:33:10 +0600

    Add `rest_framework` to `INSTALLED_APPS`
    Add `BasicAuthentication` to `REST_FRAMEWORK` settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,9 @@ MEDIA_URL = ''
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
