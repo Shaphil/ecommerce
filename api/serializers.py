9869a52b06ab7ddce0a0e4847397fb115609567b
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2024-03-28 17:04:42 +0600

    Update serializers

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,12 @@ from store.models import Cart, CartItem, Product, Order, User, DailyData
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text='Leave empty if no change needed',
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
 
     class Meta:
         model = User
@@ -20,17 +25,17 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class CartSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserSerializer()
+    # user = UserSerializer()
 
     class Meta:
         model = Cart
         fields = '__all__'
 
-    def create(self, validated_data):
-        _user = validated_data.pop('user')
-        user = User.objects.create_user(username=_user['username'], email=_user['email'])
-        cart = Cart.objects.create(user=user, **validated_data)
-        return cart
+    # def create(self, validated_data):
+    #     _user = validated_data.pop('user')
+    #     user = User.objects.create_user(username=_user['username'], email=_user['email'])
+    #     cart = Cart.objects.create(user=user, **validated_data)
+    #     return cart
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
@@ -38,13 +43,39 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
         model = Product
         fields = '__all__'
 
+    def create(self, validated_data):
+        name = validated_data.pop('name')
+        price = validated_data.pop('price')
+        image = validated_data.pop('image')
+        product = Product.objects.create(name=name, price=price, image=image)
+        return product
+
+
+class ProductGetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
 
 class CartItemSerializer(serializers.HyperlinkedModelSerializer):
-    product = ProductSerializer()
+    # product = ProductGetSerializer()
 
     class Meta:
         model = CartItem
         fields = '__all__'
+
+    # def create(self, validated_data):
+    #     product = validated_data.pop('product')
+    #     quantity = validated_data.pop('quantity')
+    #     price = validated_data.pop('price')
+    #     cart = validated_data.pop('cart')
+    #     cart_item = CartItem.objects.create(
+    #         product=product,
+    #         quantity=quantity,
+    #         price=price,
+    #         cart=cart
+    #     )
+    #     return cart_item
 
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
